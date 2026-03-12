d5a9f43512fb0c026715e3bc468849a1ab3d2ad7
sai krishna <saikrishnacheruku2848@gmail.com>
2026-03-12 23:02:39 +0530

    Streams practice 12-03-2026

--- src/Streams.java
+++ src/Streams.java
@@ -1,10 +1,12 @@
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(-9, 298, 3, 4, 57,1,2,3);
+       // List<Integer> numbers = Arrays.asList(-9, 298, 3, 4, 57,1,2,3);
 
 //        List<Integer> hs=numbers.stream().map(x->x*x).collect(Collectors.toList());
 //        System.out.println(hs);
@@ -12,7 +14,7 @@ public class Streams {
 //        List<Integer> hs= numbers.stream().filter(x->x%2==0).collect(Collectors.toList());
 //        System.out.println(hs);
 
-        List<String> words = Arrays.asList("java", "spring", "backend", "developer","baackend");
+    //    List<String> words = Arrays.asList("java", "spring", "backend", "developer","baackend");
 //
 //        List<String> res= words.stream().map(x->x.toUpperCase()).collect(Collectors.toList());
 //        System.out.println(res);
@@ -50,8 +52,52 @@ public class Streams {
 //      List<Integer> res=  numbers.stream().sorted(Comparator.reverseOrder()).skip(3).toList();
 //        System.out.println(res);
 
-        List<Integer> res=  numbers.stream().sorted().skip(3).toList();
+//        List<Integer> res=  numbers.stream().sorted().skip(3).toList();
+//        System.out.println(res);
+
+
+//        int number = 12345;
+//        Integer res=String.valueOf(number).chars().map(Character::getNumericValue).sum();
+//        System.out.println(res);
+
+//        int n=5;
+//       Integer res= IntStream.range(1,n+1).reduce((a,b)->a*b).orElse(1);
+//        System.out.println(res);
+
+
+//        Integer res=numbers.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(0);
+//        System.out.println(res);
+//
+//        Integer res1=numbers.stream().sorted().skip(1).findFirst().orElse(0);
+//        System.out.println(res1);
+
+        List<String> words = List.of("apple", "banana", "kiwi","kavi","janana");
+//
+//       String res= words.stream().min(Comparator.comparingInt(String::length)).orElse("");
+//        System.out.println(res);
+
+//        Map<Integer,List<String>> res=words.stream().collect(Collectors.groupingBy(x->x.length()));
+//
+//        System.out.println(res);
+
+        List<Person> people = List.of(
+                new Person("Alice", 25),
+                new Person("Bob", 30),
+                new Person("Charlie", 25)
+        );
+
+        Map<Integer, List<Person>> map =
+                people.stream()
+                        .collect(Collectors.groupingBy(Person::getAge));
+
+
+        List<Integer> list1 = List.of(1, 2, 3);
+        List<Integer> list2 = List.of(2, 5, 6);
+
+        List<Integer> res=Stream.concat(list2.stream(),list1.stream()).toList();
         System.out.println(res);
+
+
 
     }
 }
